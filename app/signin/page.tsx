9215c4a7e09b3a58407dd2ba6c1660d891ce797a
IonutCM <ionutcm402@gmail.com>
2026-08-23 05:47:44 +0100

    Use real authentication form on sign in

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,4 +1,1 @@
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import AuthPreview from "@/components/AuthPreview";
-export default function SignInPage(){return <div className="min-h-screen"><Header/><AuthPreview mode="signin"/><Footer/></div>}
+import Header from"@/components/Header";import Footer from"@/components/Footer";import AuthForm from"@/components/AuthForm";export const dynamic="force-dynamic";export default function SignInPage(){return <div className="min-h-screen"><Header/><AuthForm mode="signin"/><Footer/></div>}
