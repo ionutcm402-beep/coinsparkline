56e50d8cba5cd4ecefe2c18c23dd4390c91692c8
IonutCM <ionutcm402@gmail.com>
2026-08-23 05:15:39 +0100

    Add sign in page

---
+++ app/signin/page.tsx
@@ -0,0 +1,4 @@
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import AuthPreview from "@/components/AuthPreview";
+export default function SignInPage(){return <div className="min-h-screen"><Header/><AuthPreview mode="signin"/><Footer/></div>}
