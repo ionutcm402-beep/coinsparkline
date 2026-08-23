3f51affa27652f9b2673d9d686cb805206ccf7a7
IonutCM <ionutcm402@gmail.com>
2026-08-23 02:09:27 +0100

    Stage 1: establish CoinSparkLine visual design system

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="en" className="h-full antialiased">
-      <body className="min-h-full flex flex-col">
+    <html lang="en" className="h-full">
+      <body className="csl-app min-h-full flex flex-col antialiased">
         <FlowingLines />
         {children}
       </body>
