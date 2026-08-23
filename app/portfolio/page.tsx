ad3b011346abceb39a00368f5489fb793e677186
IonutCM <ionutcm402@gmail.com>
2026-08-23 05:15:11 +0100

    Add portfolio page

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,5 @@
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import PortfolioClient from "@/components/PortfolioClient";
+
+export default function PortfolioPage(){return <div className="min-h-screen"><Header/><PortfolioClient/><Footer/></div>}
