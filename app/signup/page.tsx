8d557f560d29b574a1bc38a66e4435ab1bdab01f
IonutCM <ionutcm402@gmail.com>
2026-08-23 05:15:54 +0100

    Add sign up page

---
+++ app/signup/page.tsx
@@ -0,0 +1,4 @@
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import AuthPreview from "@/components/AuthPreview";
+export default function SignUpPage(){return <div className="min-h-screen"><Header/><AuthPreview mode="signup"/><Footer/></div>}
