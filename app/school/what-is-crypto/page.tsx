f6b1a9b5ebe68a6c84cf80098460cb323dfa913d
IonutCM <ionutcm402@gmail.com>
2026-08-23 04:14:43 +0100

    Publish first Crypto School lesson

---
+++ app/school/what-is-crypto/page.tsx
@@ -0,0 +1,6 @@
+import Header from "@/components/Header";
+import SchoolLessonOne from "@/components/SchoolLessonOne";
+
+export default function WhatIsCryptoPage() {
+  return <div className="min-h-screen bg-[#fbfcff] text-slate-950"><Header /><main className="mx-auto max-w-4xl px-5 pb-20 pt-9 sm:px-8"><SchoolLessonOne /></main></div>;
+}
