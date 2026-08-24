713c4d57a1115834e9acaedff671bde252dd85ae
IonutCM <ionutcm402@gmail.com>
2026-08-24 23:03:10 +0100

    Load V1 forward visual refinements

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ import "../styles/secondary-discovery.css";
 import "../styles/trust-pages.css";
 import "../styles/accessibility.css";
 import "../styles/terminal-live.css";
+import "../styles/terminal-v1-next.css";
 import {CurrencyProvider} from "@/components/CurrencyProvider";
 import CookieConsent from "@/components/CookieConsent";
 import PWARegister from "@/components/PWARegister";
