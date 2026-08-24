cebca88ca359b025d7a9ab7b515193a18f498cb5
IonutCM <ionutcm402@gmail.com>
2026-08-24 01:29:48 +0100

    Load mobile control polish

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./mobile-polish.css";
 import FlowingLines from "@/components/FlowingLines";
 import { CurrencyProvider } from "@/components/CurrencyProvider";
 import CookieConsent from "@/components/CookieConsent";
