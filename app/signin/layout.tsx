da1d61d52f9f59bc6a050b5d928241f5e13bc534
IonutCM <ionutcm402@gmail.com>
2026-08-24 21:29:08 +0100

    Phase 12: noindex sign-in

---
+++ app/signin/layout.tsx
@@ -0,0 +1,3 @@
+import {routeMetadata} from "@/lib/seo";
+export const metadata=routeMetadata("Sign in","Sign in to your CoinSparkLine account.","/signin",{noIndex:true});
+export default function Layout({children}:{children:React.ReactNode}){return children;}
