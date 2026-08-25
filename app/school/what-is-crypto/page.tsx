650014fc488d05ca27504c556fa2b6536b5cbf70
IonutCM <ionutcm402@gmail.com>
2026-08-25 11:15:21 +0100

    Remove legacy School article

--- app/school/what-is-crypto/page.tsx
+++
@@ -1,4 +0,0 @@
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import SchoolLessonOne from "@/components/SchoolLessonOne";
-export default function WhatIsCryptoPage(){return <div className="csl-trust-page"><Header/><main className="mx-auto w-[min(860px,calc(100%-32px))] px-4 pb-20 pt-10 sm:px-0"><SchoolLessonOne/></main><Footer/></div>}
